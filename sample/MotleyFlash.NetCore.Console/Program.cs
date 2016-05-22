2e0446e01b07be1a4408658c87fa050f76658bf5
Bill Boga <bill.boga@outlook.com>
2016-05-21 20:41:51 -0400

    Add .NET Core Console sample

---
+++ sample/MotleyFlash.NetCore.Console/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace MotleyFlash.NetCore.Console
+{
+    public class Program
+    {
+        private static IMessenger messenger;
+        private static MessageTypes messageTypes = new MessageTypes();
+
+        public static void Main(string[] args)
+        {
+            var messageProvider = new InMemoryMessageProvider();
+            var messengerOptions = new MessengerOptions(messageTypes);
+
+            messenger = new StackMessenger(messageProvider, messengerOptions);
+
+            var input = string.Empty;
+
+            do
+            {
+                PrintHeader();
+
+                input = System.Console.ReadLine();
+
+                var now = System.DateTime.UtcNow;
+
+                if (input == "e")
+                {
+                    messenger.Add(new Message("An error has occured", now.ToString(), messageTypes.Error));
+                }
+                else if (input == "i")
+                {
+                    messenger.Add(new Message("This is an information message.", now.ToString(), messageTypes.Information));
+                }
+                else if (input == "s")
+                {
+                    messenger.Add(new Message("Congraulations!", now.ToString(), messageTypes.Success));
+                }
+                else if (input == "w")
+                {
+                    messenger.Add(new Message("Be careful.", now.ToString(), messageTypes.Warning));
+                }
+                else if (input == "p")
+                {
+                    PrintMessages();
+                }
+
+                System.Console.WriteLine("");
+            } while (input != "q");
+        }
+
+        public static void PrintHeader()
+        {
+            System.Console.WriteLine("(e) Add error message");
+            System.Console.WriteLine("(i) Add informational message");
+            System.Console.WriteLine("(s) Add success message");
+            System.Console.WriteLine("(w) Add warning message");
+            System.Console.WriteLine("(p) Print messages");
+            System.Console.WriteLine("(q) Quit");
+            System.Console.WriteLine("-----------------------------");
+            System.Console.Write("What is your selection? ");
+        }
+
+        public static void PrintMessages()
+        {
+            var message = messenger.Fetch();
+
+            while (message != null)
+            {
+                if (message.Type == messageTypes.Error)
+                {
+                    System.Console.ForegroundColor = System.ConsoleColor.Red;
+                }
+                else if (message.Type == messageTypes.Information)
+                {
+                    System.Console.ForegroundColor = System.ConsoleColor.Blue;
+                }
+                else if (message.Type == messageTypes.Success)
+                {
+                    System.Console.ForegroundColor = System.ConsoleColor.Green;
+                }
+                else if (message.Type == messageTypes.Warning)
+                {
+                    System.Console.ForegroundColor = System.ConsoleColor.Yellow;
+                }
+
+                System.Console.WriteLine($"[{message.Type}] {message.Title}: {message.Text}");
+
+                System.Console.ResetColor();
+
+                message = messenger.Fetch();
+            }
+        }
+    }
+}
---
+++ sample/MotleyFlash.NetCore.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MotleyFlash.NetCore.Console")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c9cd14f-20f5-4717-939a-a5313986aad2")]
